2706479e1488644f22465c254db468f8041b0e94
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2023-03-28 10:56:13 +0600

    переделал грид

--- src/components/checksList/index.tsx
+++ src/components/checksList/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../hooks/redux'
 import { IGetChecks } from '../../models/IGetChecks';
 import { chequeApi } from '../../services/ChequesService';
-import { fetchChecks } from '../../store/reducers/ActionCreator';
 import ChequeItem from '../chequeItem';
 import Error from '../error';
 import Spinner from '../spinner';
@@ -13,25 +12,13 @@ const ChecksList = () => {
 
   return (
     <div className='checksTableWrapper'>
-      {/* <div className='chequeHeader'>
-        <span className='headerItem'>Дата покупки</span>
-        <span className='headerItem'>Киоск</span>
-        <span className='headerItem'>Тип</span>
-        <span className='headerItem'>Статус оплаты</span>
-        <span className='headerItem'>Оплата</span>
-        <span className='headerItem'>Сумма</span>
-        <span className='headerItem'>Кол-во товара</span>
-        <span className='headerItem'>Товары</span>
-      </div> */}
-      <div className='chequesList'>
-        {isLoading && <Spinner />}
-        {error && <Error />}
-        {cheques?.map((cheque: IGetChecks) => (
-          <ChequeItem key={cheque.uid} cheque={cheque} />
-        ))}
-      </div>
+      {isLoading && <Spinner />}
+      {error && <Error />}
+      {cheques?.map((cheque: IGetChecks) => (
+        <ChequeItem key={cheque.uid} cheque={cheque} />
+      ))}
     </div>
   )
 }
 
-export default ChecksList
+export default ChecksList;
